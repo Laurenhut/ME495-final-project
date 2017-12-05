ad94a3ab93752affaa1bd3e09872a090a5337a01
rikago <l.hutson@live.com>
2017-12-04 19:21:13 -0600

    adjusting merger node

--- src/pose_and_item.py
+++ src/pose_and_item.py
@@ -10,50 +10,69 @@ class glue:
 	def __init__(self):
 
 		self.pose_items = rospy.Publisher("pose_and_item",ar_tagstr,queue_size = 10)
-	 	self.sbid = rospy.Subscriber("id_items",String,self.id_items)
+	 	self.sbid = rospy.Subscriber("pos_items",String,self.id_items)
 		self.ar = rospy.Subscriber("ar_pose_id",ar_tag,self.ar_info)
-		#self.list_obj = []
-		#self.id_list = []
-		#self.pose_list = []
+		self.list_obj = []
+		self.id_list = []
+		self.pose_list = []
 		
 	
 
 	def id_items(self,strid):
-		self.list_obj = strid.split()
-		print "th2"
+		
+		self.list_obj = strid.data.split(' ')
+		
+
+
+	
+		
 
 	def ar_info(self,ar_msg):
-	    ar = ar_tagstr()
-	    ar.id="th"
-	    ar.pose=ar_msg.pose
-	    self.pose_items.publish(ar)
-	    print "th"
 	    
-	
-		if len(self.id_list) < 3:
+	    
+		if len(self.id_list) < 3 and self.list_obj is not None:
 			self.id_list.append(ar_msg.id)
 			self.pose_list.append(ar_msg.pose)
-		if len(self.id_list) == 3 and self.id_list[0] != self.id_list[1] and self.id_list[0] != self.id_list[2] and self.id_list[1] != self.id_list[2]:
+			print len(self.pose_list),len(self.pose_list)
+
+		if len(self.id_list) == 3 and (self.id_list[0] != self.id_list[1]) and (self.id_list[0] != self.id_list[2]) and (self.id_list[1] != self.id_list[2]):
 			ar = ar_tagstr()
-			y0 = self.pose_list[0].Point.y
-			y1 = self.pose_list[1].Point.y
-			y2 = self.pose_list[2].Point.y
-			pos = np.argsort([y0,y1,y2])
-			pos = list(pos)
+	    	
+	    	y0 = self.pose_list[0].position.y
+	    	y1 = self.pose_list[1].position.y
+	    	y2 = self.pose_list[2].position.y
+	    	print self.pose_list[2]
+	    	pos = np.argsort([y0,y1,y2])
+	    	pos = list(pos)
 
-			ind0 = pos.index(0)
-			ind1 = pos.index(1)
-			ind2 = pos.index(2)
+	    	ind0 = pos.index(0)
+	    	ind1 = pos.index(1)
+	    	ind2 = pos.index(2)
 
-			ar.id[ind0] = self.id_list[0]
-			ar.pose[ind0] = self.pose_list[0]
-			ar.id[ind1] = self.id_list[1]
-			ar.pose[ind1] = self.pose_list[1]
-			ar.id[ind2] = self.id_list[2]
-			ar.pose[ind2] = self.pose_list[2]
-			#self.pose_items.publish(ar)
-			self.id_list = None
-			self.pose = None
+	    	 
+	    	ar.id=self.list_obj[2]
+	    	
+	    	ar.pose=self.pose_list[2]
+	    	print ar
+	    	#self.pose_items.publish(ar)
+
+	    	# ar.id[ind0] = self.id_list[0]
+	    	# ar.pose[ind0] = self.pose_list[0]
+	    	# ar.id[ind1] = self.id_list[1]
+	    	# ar.pose[ind1] = self.pose_list[1]
+	    	# ar.id[ind2] = self.id_list[2]
+	    	# ar.pose[ind2] = self.pose_list[2]
+	    	# self.pose_items.publish(ar)
+	    	self.id_list = []
+	    	self.pose_list =[]
+    	
+
+
+
+
+
+	    
+		
 		# 	x=1
 
 def main():
